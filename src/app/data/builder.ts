a08f488bfed7fa9350a223c26d1fbade2e03b8f5
AdaNostalgic <seb.filhol@gmail.com>
2023-09-15 22:00:59 +0200

    2 files modified, 2 added, 1 deleted

---
+++ src/app/data/builder.ts
@@ -0,0 +1,19 @@
+export interface Building {
+  name: string;
+  power: number;
+}
+
+export const buildings = [
+  {
+    name: 'smelter',
+    power: 4,
+  },
+  {
+    name: 'constructor',
+    power: 4,
+  },
+  {
+    name: 'assembler',
+    power: 15,
+  },
+];
---
+++ src/app/data/products.ts
@@ -0,0 +1,50 @@
+export interface Product {
+  name: string;
+  building: string;
+  outProductNb: number;
+  inProducts: {
+    name: string;
+    nb: number;
+  }[];
+}
+
+export const products = [
+  {
+    name: 'ironOre',
+    building: 'miner',
+    outProductNb: 60,
+  },
+  {
+    name: 'ironLingot',
+    building: 'smelter',
+    outProductNb: 30,
+    inProducts: [{ name: 'ironOre', nb: 30 }],
+  },
+  {
+    name: 'ironPlate',
+    building: 'constructor',
+    outProductNb: 20,
+    inProducts: [{ name: 'ironLingot', nb: 30 }],
+  },
+  {
+    name: 'ironRod',
+    building: 'constructor',
+    outProductNb: 20,
+    inProducts: [{ name: 'ironLingot', nb: 30 }],
+  },
+  {
+    name: 'screw',
+    building: 'constructor',
+    outProductNb: 40,
+    inProducts: [{ name: 'ironRod', nb: 10 }],
+  },
+  {
+    name: 'reinforcedIronPlate',
+    building: 'assembler',
+    outProductNb: 5,
+    inProducts: [
+      { name: 'ironPlate', nb: 30 },
+      { name: 'screw', nb: 60 },
+    ],
+  },
+];
